8db05d7d683fb653714949de80fc6e3eb1fe4055
Gabriele Polenta <g.polenta@orderfox.com>
2020-05-23 19:32:33 +0200

    product of other numbers

--- product_of_other_numbers.py
+++ product_of_other_numbers.py
@@ -28,7 +28,6 @@ Solution 2:
   Time complexity: O(n)
   Space complexity: O(n)
 '''
-import unittest
 
 # Solution 2
 def get_products_of_all_ints_except_at_index(my_list):
@@ -52,46 +51,3 @@ def get_products_of_all_ints_except_at_index(my_list):
 
 my_input = [1, 7, 5, 6, 2]
 print(get_products_of_all_ints_except_at_index(my_input))
-
-class Test(unittest.TestCase):
-
-  def test_small_list(self):
-    actual = get_products_of_all_ints_except_at_index([1, 2, 3])
-    expected = [6, 3, 2]
-    self.assertEqual(actual, expected)
-
-  def test_longer_list(self):
-    actual = get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5])
-    expected = [120, 480, 240, 320, 960, 192]
-    self.assertEqual(actual, expected)
-
-  def test_list_has_one_zero(self):
-    actual = get_products_of_all_ints_except_at_index([6, 2, 0, 3])
-    expected = [0, 0, 36, 0]
-    self.assertEqual(actual, expected)
-
-  def test_list_has_two_zeros(self):
-    actual = get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
-    expected = [0, 0, 0, 0, 0]
-    self.assertEqual(actual, expected)
-
-  def test_one_negative_number(self):
-    actual = get_products_of_all_ints_except_at_index([-3, 8, 4])
-    expected = [32, -12, -24]
-    self.assertEqual(actual, expected)
-
-  def test_all_negative_numbers(self):
-    actual = get_products_of_all_ints_except_at_index([-7, -1, -4, -2])
-    expected = [-8, -56, -14, -28]
-    self.assertEqual(actual, expected)
-
-  def test_error_with_empty_list(self):
-    with self.assertRaises(Exception):
-      get_products_of_all_ints_except_at_index([])
-
-  def test_error_with_one_number(self):
-    with self.assertRaises(Exception):
-      get_products_of_all_ints_except_at_index([1])
-
-
-unittest.main(verbosity=2)
