fe5ab213865238604ba00fcc9a9656f08ef3913e
Gabriele Polenta <g.polenta@orderfox.com>
2020-05-30 11:46:10 +0200

    reverse string in place

--- reverse_string_in_place.py
+++ reverse_string_in_place.py
@@ -1,11 +1,12 @@
 # Write a function that takes a list of characters and reverses the letters in place.
 
+
 def reverse_characters(chars):
-  for i in range(len(chars) // 2):
-    temp = chars[i]
-    chars[i] = chars[-1 - i]
-    chars[-1 - i] = temp
-  return chars
+    for i in range(len(chars) // 2):
+        temp = chars[i]
+        chars[i] = chars[-1 - i]
+        chars[-1 - i] = temp
+    return chars
 
 
 my_chars = ['h', 'e', 'l', 'l', 'o']
