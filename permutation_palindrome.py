bccbfb7a96276cc90e8280f9993aa69e62bfd264
Gabriele Polenta <g.polenta@orderfox.com>
2020-05-30 11:47:21 +0200

    permutation palindrome

--- permutation_palindrome.py
+++ permutation_palindrome.py
@@ -19,29 +19,32 @@ A brute force approach would be very expensive (n!n) in term of runtime.
 I can do better than that using a hash map (Python dictionary) and store the occurrences of each char in the input string. If more than one char has an odd occurrence, then no permutation of the input string is a palindrome. Using this approach, the time complexity will be linear and the space complexity constant (read below after the solution).
 '''
 
+
 def has_palindrome_permutation(the_string):
-  my_dict = {}
+    my_dict = {}
 
-  for char in the_string:
-    if char in my_dict:
-      my_dict[char] += 1
+    for char in the_string:
+        if char in my_dict:
+            my_dict[char] += 1
+        else:
+            my_dict[char] = 1
+    print(my_dict)
+
+    chars_with_odd_occurrences = 0
+    for char in my_dict:
+        if my_dict[char] % 2 != 0:
+            chars_with_odd_occurrences += 1
+
+    if chars_with_odd_occurrences > 1:
+        return False
     else:
-      my_dict[char] = 1
-  print(my_dict)
-
-  chars_with_odd_occurrences = 0
-  for char in my_dict:
-    if my_dict[char] % 2 != 0:
-      chars_with_odd_occurrences += 1
-
-  if chars_with_odd_occurrences > 1:
-    return False
-  else:
-    return True
+        return True
 
 
-my_string = "aabbc" #returns True because only one letter ("k") has an odd number of occurrences.
-my_string = "aabbcd" #returns False because more than one char ("c" and "d") have an odd number of occurrences.
+# returns True because only one letter ("k") has an odd number of occurrences.
+my_string = "aabbc"
+# returns False because more than one char ("c" and "d") have an odd number of occurrences.
+my_string = "aabbcd"
 print(has_palindrome_permutation(my_string))
 
 '''
